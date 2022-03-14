e90b19b51dffb7ec8d8a98d109dfc7c52aa3f7e5
Ana Cristina Leme <90112198+CrisLeme@users.noreply.github.com>
2022-03-14 10:46:58 -0300

    Add files via upload

---
+++ Desafio 2.py
@@ -0,0 +1,7 @@
+print("Meu animal favorito é um gato")
+print(" ^0^###-")
+print("    | | ")
+
+
+print("Eu moro em Teresina")
+print(" ---<0>--- ")
---
+++ Dinoassauro.py
@@ -0,0 +1,17 @@
+print("Parabéns" + "!"*10)
+print("Aqui vai um dinossauro!")
+
+print("              __  ")
+print("             / _) ")
+print("      .-^^^-/ /   ")
+print("    _/       /    ")
+print("   <_.|_|-|_|     ")
+
+print(" E aqui vai um bolo! hummmm")
+
+print("i" * 10)
+print("#" * 12)
+print("=" * 12)
+print("#" * 12)
+print("=" * 12)
+print("#" * 12)
